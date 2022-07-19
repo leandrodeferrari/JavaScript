306e596fe076363d8d9e1534f2722c57df82f318
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-07-18 21:09:14 -0300

    Resuelto el ejercicio 3

---
+++ ejercicio3/script.js
@@ -0,0 +1,10 @@
+/*
+
+    3. Escriba un programa en donde se pida la edad del usuario. Si el usuario es mayor de 
+    edad se debe mostrar un mensaje indicándolo. 
+
+*/
+
+let edadUsuario = prompt("¿Qué edad tenés?",18);
+
+(edadUsuario>=18) ? alert("Sos mayor de edad") : alert("Sos menor de edad");
